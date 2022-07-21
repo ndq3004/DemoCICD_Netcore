0be464b38e39336c80c4cb3dfab8bbc9436da88e
ndq3004 <ndq3004@gmail.com>
2022-07-21 17:00:51 +0700

    f

--- DemoUnitTestPipeline/UnitTest1.cs
+++ DemoUnitTestPipeline/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace DemoUnitTestPipeline
         {
             var controller = new WeatherForecastController();
             var result = controller.Get();
-            Assert.IsNull(result);
+            Assert.IsNotNull(result);
         }
     }
 }
--- Jenkinsfile
+++ Jenkinsfile
@@ -6,7 +6,7 @@ pipeline {
         stage("build"){
 
             steps {
-                echo "build"
+                dotnet build -c Release
             }
         }
 
