753972468ec8a3e8f2027eefb9a149afbde4bfd9
ndq3004 <ndq3004@gmail.com>
2022-07-21 16:30:39 +0700

    d

--- DemoCICDSolution/Controllers/WeatherForecastController.cs
+++ DemoCICDSolution/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DemoCICDSolution.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching1223"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching12234"
     };
 
         //private readonly ILogger<WeatherForecastController> _logger;
