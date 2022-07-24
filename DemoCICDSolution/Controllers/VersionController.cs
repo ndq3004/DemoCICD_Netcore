ca8d9f7931c36b8fc74835521e3f88932d8cc94f
ndq3004 <ndq3004@gmail.com>
2022-07-25 01:33:41 +0700

    add version

---
+++ DemoCICDSolution/Controllers/VersionController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoCICDSolution.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController : ControllerBase
+    {
+        private IConfiguration Configuration;
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching1223456"
+    };
+
+        //private readonly ILogger<WeatherForecastController> _logger;
+
+        public VersionController(IConfiguration configuration)
+        {
+            //_logger = logger;
+            Configuration = configuration;
+        }
+
+        [HttpGet(Name = "Version")]
+        public string Get()
+        {
+            return Configuration["Version"];
+        }
+    }
+}
